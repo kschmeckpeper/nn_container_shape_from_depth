e6e809e41c35f43d27cd4e52fb9f38b167028fa7
Karl Schmeckpeper <karl.schmeckpeper@gmail.com>
2018-08-01 14:59:56 -0400

    Added max pooling

--- models/conv_net.py
+++ models/conv_net.py
@@ -8,51 +8,59 @@ class ConvNet(nn.Module):
                  input_image_size=128,
                  num_output_channels=128, 
                  num_hidden_channels=1024,
-                 stride=2,
-                 kernel=5,
+                 max_pooling_kernel=2,
+                 conv_kernel=5,
                  num_linear_layers=3,
                  dropout_prob=0.0,
 
                  nonlinearity='ReLU'):
         super(ConvNet, self).__init__()
-        print "Stride", stride
-        layers = OrderedDict()
+
+        conv_layers = OrderedDict()
         
-        layers['conv_0'] = nn.Conv2d(1, num_hidden_channels, kernel, stride=stride)
-        print "Created layer"
-        num_conv_layers = 0
-        while input_image_size > 1:
-            input_image_size = (input_image_size - kernel - 1) / stride + 1
-            num_conv_layers += 1
-        print "Num conv layers", num_conv_layers
+        conv_layers['conv_0'] = nn.Conv2d(1, num_hidden_channels, conv_kernel)
+        num_conv_layers = 5
+
+        # num_conv_layers = 0
+        # while input_image_size > 1:
+        #     input_image_size = (input_image_size - kernel - 1) / stride + 1
+        #     num_conv_layers += 1
+
+        # print "Num conv layers", num_conv_layers
         for i in range(1, num_conv_layers):
-            layers[nonlinearity + '_conv_' + str(i)] = ConvNet._get_nonlinearity(nonlinearity)
-            layers['dropout_conv_' + str(i)] = nn.Dropout(p=dropout_prob)
-            layers['conv_' + str(i)] = nn.Conv2d(num_hidden_channels, num_hidden_channels, kernel, stride=stride)
+            conv_layers[nonlinearity + '_conv_' + str(i)] = ConvNet._get_nonlinearity(nonlinearity)
+            conv_layers['dropout_conv_' + str(i)] = nn.Dropout(p=dropout_prob)
+            conv_layers['conv_' + str(i)] = nn.Conv2d(num_hidden_channels, num_hidden_channels, conv_kernel)
+            conv_layers['pooling_' + str(i)] = nn.MaxPool2D(max_pooling_kernel)
+        self.conv_network = nn.Sequential(conv_layers)
+        self.conv_layers = conv_layers
 
-
+        linear_layers = OrderedDict()
         for i in range(num_linear_layers):
-            layers[nonlinearity + '_linear_' + str(i)] = ConvNet._get_nonlinearity(nonlinearity)
+            linear_layers[nonlinearity + '_linear_' + str(i)] = ConvNet._get_nonlinearity(nonlinearity)
             
-            layers['dropout_linear_' + str(i)] = nn.Dropout(p=dropout_prob)
-            layers['linear_' + str(i)] = nn.Linear(num_hidden_channels, num_hidden_channels)
+            linear_layers['dropout_linear_' + str(i)] = nn.Dropout(p=dropout_prob)
+            linear_layers['linear_' + str(i)] = nn.Linear(num_hidden_channels, num_hidden_channels)
 
-        layers[nonlinearity + '_final'] = ConvNet._get_nonlinearity(nonlinearity)
-        layers['output'] = nn.Linear(num_hidden_channels, num_output_channels)
-        self.network = nn.Sequential(layers)
-        self.layers = layers
+        linear_layers[nonlinearity + '_final'] = ConvNet._get_nonlinearity(nonlinearity)
+        linear_layers['output'] = nn.Linear(num_hidden_channels, num_output_channels)
+        self.fully_connected_network = nn.Sequential(linear_layers)
+
 
 
     def forward(self, x):
         print "input", x.shape
-        for layer in self.layers:
-            print "Layer:", layer
-            if layer == 'linear_0':
-                x = x.view(-1, 1024)
-            x = self.layers[layer](x)
+        # x = self.conv_network(x)
+        for layer in self.conv_layers:
+            print "Layer", layer
+            x = self.conv_layers['layer'](x)
             print "x", x.shape
+
+        x = x.view(-1, self.num_hidden_channels)
+        x = self.fully_connected_network(x)
+
         return x + 1.0
-        #return self.network(x) + 1.0
+        
 
     @staticmethod
     def _get_nonlinearity(nonlinearity):
