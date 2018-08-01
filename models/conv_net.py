f5b93a6658229b0bc2f3eefd62bc4f669213e543
Karl Schmeckpeper <karls@seas.upenn.edu>
2018-08-01 15:15:31 -0400

    Fixed dimensionality

--- models/conv_net.py
+++ models/conv_net.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import numpy as np
-import torch.nn.functional as F
+import math
 from collections import OrderedDict
 
 class ConvNet(nn.Module):
@@ -12,26 +12,20 @@ class ConvNet(nn.Module):
                  conv_kernel=5,
                  num_linear_layers=3,
                  dropout_prob=0.0,
-
                  nonlinearity='ReLU'):
         super(ConvNet, self).__init__()
-
+        self.num_hidden_channels = num_hidden_channels
         conv_layers = OrderedDict()
         
-        conv_layers['conv_0'] = nn.Conv2d(1, num_hidden_channels, conv_kernel)
-        num_conv_layers = 5
+        conv_layers['conv_0'] = nn.Conv2d(1, num_hidden_channels, conv_kernel, padding=(conv_kernel-1)/2)
+        conv_layers['pooling_0'] = nn.MaxPool2d(max_pooling_kernel)
+        num_conv_layers = int(math.ceil(math.log(128, 2)))
 
-        # num_conv_layers = 0
-        # while input_image_size > 1:
-        #     input_image_size = (input_image_size - kernel - 1) / stride + 1
-        #     num_conv_layers += 1
-
-        # print "Num conv layers", num_conv_layers
         for i in range(1, num_conv_layers):
             conv_layers[nonlinearity + '_conv_' + str(i)] = ConvNet._get_nonlinearity(nonlinearity)
             conv_layers['dropout_conv_' + str(i)] = nn.Dropout(p=dropout_prob)
-            conv_layers['conv_' + str(i)] = nn.Conv2d(num_hidden_channels, num_hidden_channels, conv_kernel)
-            conv_layers['pooling_' + str(i)] = nn.MaxPool2D(max_pooling_kernel)
+            conv_layers['conv_' + str(i)] = nn.Conv2d(num_hidden_channels, num_hidden_channels, conv_kernel, padding=(conv_kernel-1)/2)
+            conv_layers['pooling_' + str(i)] = nn.MaxPool2d(max_pooling_kernel)
         self.conv_network = nn.Sequential(conv_layers)
         self.conv_layers = conv_layers
 
@@ -49,12 +43,12 @@ class ConvNet(nn.Module):
 
 
     def forward(self, x):
-        print "input", x.shape
-        # x = self.conv_network(x)
-        for layer in self.conv_layers:
-            print "Layer", layer
-            x = self.conv_layers['layer'](x)
-            print "x", x.shape
+        #print "input", x.shape
+        x = self.conv_network(x)
+        #for layer in self.conv_layers:
+        #    print "Layer", layer
+        #    x = self.conv_layers[layer](x)
+        #    print "x", x.shape
 
         x = x.view(-1, self.num_hidden_channels)
         x = self.fully_connected_network(x)
