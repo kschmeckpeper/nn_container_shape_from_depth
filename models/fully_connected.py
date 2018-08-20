54e33daddb60aeaddd2b0589572f4154964d2094
Karl Schmeckpeper <karl.schmeckpeper@gmail.com>
2018-08-20 14:17:07 -0400

    Allowed fully connected to handle speed and angle

--- models/fully_connected.py
+++ models/fully_connected.py
@@ -12,12 +12,15 @@ class FullyConnected(nn.Module):
                  num_hidden_layers=10,
                  dropout_prob=0.0,
                  use_batch_norm=False,
+                 use_speed_and_angle=False,
                  nonlinearity='ReLU'):
         super(FullyConnected, self).__init__()
 
         if not type(num_hidden_channels) is list:
             num_hidden_channels = [num_hidden_channels] * (num_hidden_layers + 1)
 
+        if use_speed_and_angle:
+            num_input_channels += 2
 
         layers = OrderedDict()
         layers['input'] = nn.Linear(num_input_channels, num_hidden_channels[0])
@@ -35,4 +38,8 @@ class FullyConnected(nn.Module):
         self.network = nn.Sequential(layers)
 
     def forward(self, x):
+        return self.network(x) + 1.0
+
+    def forward(self, x, speed, angle):
+        x = torch.cat((x, speed.view(speed.shape[0], 1), angle.view(angle.shape[0], 1)), dim=1)
         return self.network(x) + 1.0
